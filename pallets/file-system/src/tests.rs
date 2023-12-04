317dae15ebffab27f7bcc48bf189ead62f6ab760
ffarall <37149322+ffarall@users.noreply.github.com>
2023-12-04 15:30:02 -0800

    test: :white_check_mark: Add success test cases for registering storage request and BSP volunteering

--- pallets/file-system/src/tests.rs
+++ pallets/file-system/src/tests.rs
@@ -1,27 +1,71 @@
-use crate::{mock::*, Error, Event};
-use frame_support::{assert_noop, assert_ok};
+use crate::{mock::*, types::FileLocation, Event};
+use frame_support::assert_ok;
+use sp_runtime::traits::{BlakeTwo256, Hash};
 
 #[test]
-fn it_works_for_default_value() {
+fn request_storage_success() {
 	new_test_ext().execute_with(|| {
 		// Go past genesis block so events get deposited
 		System::set_block_number(1);
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(RuntimeOrigin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
+
+		let user = RuntimeOrigin::signed(1);
+		let location = FileLocation::<Test>::try_from(b"test".to_vec()).unwrap();
+		let file_content = b"test".to_vec();
+		let content_id = BlakeTwo256::hash(&file_content);
+
+		// Dispatch storage request.
+		assert_ok!(FileSystem::request_storage(
+			user.clone(),
+			location.clone(),
+			content_id.clone(),
+			4,
+			1
+		));
+
 		// Assert that the correct event was deposited
-		System::assert_last_event(Event::SomethingStored { something: 42, who: 1 }.into());
+		System::assert_last_event(
+			Event::NewStorageRequest {
+				who: 1,
+				location,
+				content_id,
+				size: 4,
+				sender_multiaddress: 1,
+			}
+			.into(),
+		);
 	});
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn bsp_volunteer_success() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			TemplateModule::cause_error(RuntimeOrigin::signed(1)),
-			Error::<Test>::NoneValue
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+
+		let user = RuntimeOrigin::signed(1);
+		let bsp = RuntimeOrigin::signed(2);
+		let location = FileLocation::<Test>::try_from(b"test".to_vec()).unwrap();
+		let file_content = b"test".to_vec();
+		let content_id = BlakeTwo256::hash(&file_content);
+
+		// Register BSP in Identity Pallet.
+		assert_ok!(Identity::register_user(RuntimeOrigin::root(), 2));
+
+		// Dispatch storage request.
+		assert_ok!(FileSystem::request_storage(
+			user.clone(),
+			location.clone(),
+			content_id.clone(),
+			4,
+			1
+		));
+
+		// Dispatch BSP volunteer.
+		assert_ok!(FileSystem::bsp_volunteer(bsp.clone(), location.clone(), content_id.clone(), 2));
+
+		// Assert that the correct event was deposited
+		System::assert_last_event(
+			Event::NewBspVolunteer { who: 2, location, content_id, bsp_multiaddress: 2 }.into(),
 		);
 	});
 }
